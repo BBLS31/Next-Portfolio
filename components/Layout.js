88cd8219f697e2634f059f4dd04408d24de8905f
BBLS31 <101740031+BBLS31@users.noreply.github.com>
2024-01-29 21:19:48 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -2,42 +2,14 @@ import React from "react";
 import Navbar from "./Navbar";
 import Head from "next/head";
 import { useEffect } from "react";
-import $ from "jquery";
+import GoogleAnalytics from "./Analytics";
 
 
 function Layout(props) {
-
-useEffect(() => {
-  $(document).ready(function () {
-    // Add smooth scrolling to all links
-    $("a").on("click", function (event) {
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-        // Store hash
-        var hash = this.hash;
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $("html, body").animate(
-          {
-            scrollTop: $(hash).offset().top,
-          },
-          800,
-          function () {
-            // Add hash (#) to URL when done scrolling (default click behavior)
-            window.location.hash = hash;
-          }
-        );
-      } // End if
-    });
-  });
-
-}, []);
-
   return (
     <div className="page-layout">
       <Head>
+        <GoogleAnalytics TRACKING_ID={process.env.TRACKING_ID} />
         <link
           rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
