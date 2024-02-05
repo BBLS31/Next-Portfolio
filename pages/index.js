b87ff94bcf8d088b44c26d212143fc2a727907a3
BBLS31 <101740031+BBLS31@users.noreply.github.com>
2024-02-05 05:10:03 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,58 @@ function Welcome(props) {
           border: 2px solid grey;
           border-radius: 10px;
         }
+
+        /* Media query for medium desktop screens */
+        @media (min-width: 992px) and (max-width: 1199px) {
+          .container{
+            height: 100vh
+            width: 100%
+          }
+          h1 {
+            font-size: 84px;
+            text-align: center;
+            width: 100%
+          }
+        }
+
+        /* Media query for small desktop screens */
+        @media (min-width: 768px) and (max-width: 991px) {
+          .container{
+            height: 100vh;
+            width: 100%;
+          }
+          h1 {
+            font-size: 62px;
+            text-align: center;
+            width: 100%
+          }
+        }
+
+        /* Media query for tablet devices */
+        @media (max-width: 768px) {
+          .container {
+            height: 100vh;
+            width: 100%;
+          }
+          h1 {
+            font-size: 46px;
+            text-align: center;
+            width: 100%
+          }
+        }
+
+        /* Media query for mobile devices */
+        @media (max-width: 480px) {
+          .container{
+            height: 100vh;
+            width: 100%
+          }
+          h1 {
+            font-size: 30px;
+            text-align: center;
+            width: 100%
+          }
+        }
       `}</style>
     </Layout>
   );
